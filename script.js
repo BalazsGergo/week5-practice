33073a230d96ab4680df8915596db7b4857c1199
Balazs Gergo <gergobalazs98@gmail.com>
2023-10-26 10:44:28 +0200

    fetch component added

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const rootElement = document.querySelector("#root")
 
 console.dir(rootElement)
 
+const fetchUrl = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+   // console.log(data)
+    return data
+}
+
 
 const countryComponent = (country) => `
     <div class="country">
@@ -13,11 +20,13 @@ const countryComponent = (country) => `
 `
 
 async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
-    const response = await fetch("https://restcountries.com/v3.1/all") // megvárjuk a fetch válaszát (promise helyett)
-    const data = await response.json() // megvárjuk, hogy a response megjöjjön (promise helyett) -> itt lesz elérhető az adatunk
-
-
-    data.forEach(country => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)))
+     const data = await fetchUrl("https://restcountries.com/v3.1/all")
+     console.log(data)
+    
+     data.forEach(country => rootElement.insertAdjacentHTML("beforeend",countryComponent(country)))
+    // megvárjuk a fetch válaszát (promise helyett)
+    // megvárjuk, hogy a response megjöjjön (promise helyett) -> itt lesz elérhető az adatunk
+    
 }
 
 init()
