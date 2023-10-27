2cef2dc208eef7a171bb3a32419bf8b510fe2343
Balazs Gergo <gergobalazs98@gmail.com>
2023-10-27 17:30:22 +0200

    practice

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ const fetchUrl = async (url) => {
     return data
 }
 
+const skeletonComponent = () =>`
+    <headre></header>
+    <main></main>
+    <footer></footer>
+`
+
 
 const countryComponent = (country) => `
     <div class="country">
@@ -19,17 +25,22 @@ const countryComponent = (country) => `
     </div>
 `
 
+const makeDomFromData = (data, rootElement, componentFn) => {
+    data.forEach(value => rootElement.insertAdjacentHTML("beforeend", componentFn(value)))
+}
+
+
+
 async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
+    rootElement.insertAdjacentElement("beforeend", skeletonComponent())
+    const mainElement = Document.querySelector("main")
+
      const data = await fetchUrl("https://restcountries.com/v3.1/all")
      console.log(data)
     
-     data.forEach(country => rootElement.insertAdjacentHTML("beforeend",countryComponent(country)))
+     makeDomFromData(data,rootElement, countryComponent)
     
-   /*  const countryElement = document.querySelector("div.country")
-    console.log(countryElement)
-    countryElement.addEventListener("click", () =>{
-        countryElement.classList.toggle("click")
-    }) */
+   
 
     const countryElements = document.querySelectorAll("div.country")
     countryElements.forEach((countryElement) => {
